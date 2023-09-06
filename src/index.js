a8670043e0b09138107ac813c33864ab0aa12ed1
Alexander Suslin <asuslin01@gmail.com>
2023-09-06 17:48:15 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+import React, { StrictMode } from 'react';
+
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
